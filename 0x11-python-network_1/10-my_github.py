9cf69ab83d5cdad4e8e3128a4c0b9e673891a0e2
Akintayo <97290223+mumzkid@users.noreply.github.com>
2024-03-01 14:38:37 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""Prints the GitHub id of a user."""
-import sys
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
 import requests
-
+from requests.auth import HTTPBasicAuth
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        url = "https://api.github.com/user"
-        username = sys.argv[1]
-        password = sys.argv[2]
-        req_headers = {
-            'Accept': 'application/vnd.github.v3+json',
-            'Username': username,
-            'Authorization': 'token {}'.format(password),
-        }
-        response = requests.get(url, headers=req_headers)
-        if response.status_code == 200:
-            user = response.json()
-            if user['login'] == username:
-                print(user['id'])
-            else:
-                print('None')
-        else:
-            print('None')
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
