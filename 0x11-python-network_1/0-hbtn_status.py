7b9c967bbf356cfee7eb43a3d13c14c9f01b6005
Akintayo <97290223+mumzkid@users.noreply.github.com>
2024-03-01 15:07:41 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""This script fetches data from https://alx-intranet.hbtn.io/status"""
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
 
 import urllib.request
-import urllib.error
 
-def fetch_data(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read()
-            return body.decode('utf-8')
-    except urllib.error.URLError as e:
-        print(f"Failed to fetch data: {e}")
-        return None
-
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    data = fetch_data(url)
-    if data is not None:
-        print("Body response:")
-        print(f"\t- type: {type(data)}")
-        print(f"\t- content: {data}")
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
