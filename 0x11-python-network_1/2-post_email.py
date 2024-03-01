ca8fdd455d9b5297c60c3ed4a93183bf8b826243
Akintayo <97290223+mumzkid@users.noreply.github.com>
2024-03-01 14:57:43 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,18 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    if len(sys.argv) != 3:
+        print("Usage: python3 script.py <URL> <email>")
+        sys.exit(1)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as resp:
-        print(resp.read().decode("utf-8"))
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
+
+    try:
+        request = urllib.request.Request(url, data)
+        with urllib.request.urlopen(request) as resp:
+            print(resp.read().decode("utf-8"))
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
